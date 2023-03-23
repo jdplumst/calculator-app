b67f3e63e2c8a101ec460edbfce772d1e923c5d6
Jeremiah Plumstead <jeremiah.plumstead@gmail.com>
2023-03-23 06:28:10 -0400

    Add API route for signup

---
+++ pages/api/signup.ts
@@ -0,0 +1,40 @@
+import { NextApiRequest, NextApiResponse } from "next";
+import bcrypt from "bcrypt";
+import prisma from "@/prisma";
+
+export default async (req: NextApiRequest, res: NextApiResponse) => {
+  // Ensure only POST methods can be used on this route
+  switch (req.method) {
+    case "POST":
+      const { username, password } = req.body;
+
+      // Make sure both username and password are non-empty
+      if (!username || !password) {
+        return res.status(400).json({ error: "All fields must be filled" });
+      }
+
+      // Create a hash for password with salt
+      const salt = await bcrypt.genSalt();
+      const hash = await bcrypt.hash(password, salt);
+
+      // Add new user to db, catch if user already exists in db or other errors
+      try {
+        const user = await prisma.users.create({
+          data: { username: username, password: hash }
+        });
+        return res.status(200).json({ user: user });
+      } catch (error: any) {
+        if (error.code === "P2002") {
+          return res.status(400).json({ error: "Username is already in use" });
+        } else {
+          return res
+            .status(400)
+            .json({ error: "Something went wrong. Try again." });
+        }
+      }
+
+    // Send error message if other methods used
+    default:
+      return res.status(405).json({ error: "Method Not Allowed" });
+  }
+};
