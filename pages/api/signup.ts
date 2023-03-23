3f62489aabea905abd166a2a6da7af58eeb9abf0
Jeremiah Plumstead <jeremiah.plumstead@gmail.com>
2023-03-23 09:10:58 -0400

    Generate JWT on signup

--- pages/api/signup.ts
+++ pages/api/signup.ts
@@ -1,6 +1,8 @@
 import { NextApiRequest, NextApiResponse } from "next";
 import bcrypt from "bcrypt";
 import prisma from "@/prisma";
+import jsonwebtoken from "jsonwebtoken";
+import { serialize } from "cookie";
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
   // Ensure only POST methods can be used on this route
@@ -17,13 +19,27 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
       const salt = await bcrypt.genSalt();
       const hash = await bcrypt.hash(password, salt);
 
-      // Add new user to db, catch if user already exists in db or other errors
       try {
+        // Add new user to db and create JWT
         const user = await prisma.users.create({
           data: { username: username, password: hash }
         });
-        return res.status(200).json({ username: username });
+        const token = jsonwebtoken.sign(
+          { id: user.id },
+          process.env.TOKEN_SECRET as string,
+          { expiresIn: 60 * 60 } // expires in 1 hour
+        );
+        res.setHeader(
+          "Set-Cookie",
+          serialize("token", token, {
+            httpOnly: true,
+            secure: process.env.NODE_ENV !== "development",
+            maxAge: 60 * 60
+          })
+        );
+        return res.status(200).json({ username: username, token: token });
       } catch (error: any) {
+        // Catch if user already exists in db or other errors
         if (error.code === "P2002") {
           return res.status(400).json({ error: "Username is already in use" });
         } else {
